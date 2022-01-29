ef91831fc7cdb7e881fd122a77eb627b366f1bd9
nazaninshahrad <nazanin.shahrad@gmail.com>
2022-01-28 20:48:26 -0500

    add functions to contentlist class

---
+++ src/app/content-card/content-card.component.ts
@@ -0,0 +1,61 @@
+import { leadingComment } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { ContentList } from '../helper-files/content-list';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.css']
+})
+export class ContentCardComponent implements OnInit {
+
+
+ item1:Content = {
+  id : 1,
+  title : "Apple Cake",
+  description : "How to make Apple Cake",
+  creator : "nazanin",
+  imgURL : "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/german_apple_cake_59663_16x9.jpg",
+  type : "Fruit Cakes",
+  tags : ["cake" , "friut cake"],
+
+};
+
+item2:Content = {
+  id : 2,
+  title : "Sponge Cake",
+  description : "how to make Sponge Cake",
+  creator : "nazanin",
+  imgURL : "https://cdn.momsdish.com/wp-content/uploads/2020/02/Perfect-Sponge-Cake-12-1200x800.jpg",
+  type : "Cake",
+  tags : ["cake" , "sponge cake"],
+
+};
+
+item3:Content = {
+  id : 3,
+  title : "Chocolate Cake",
+  description : "How to make Chocolate Cake",
+  creator : "nazanin",
+  imgURL : "https://preppykitchen.com/wp-content/uploads/2019/06/Ultimate-Chocolate-Cake-feature-1200.jpg",
+  type : "cake",
+  tags : ["cake"],
+
+};
+
+ contentList = new ContentList();
+  constructor() {
+
+    console.log('hello');
+   this.contentList.addContent(this.item1);
+    this.contentList.addContent(this.item2);
+    this.contentList.addContent(this.item3);
+    console.log(this.contentList.getContectCard(1));
+  }
+
+  ngOnInit(): void {
+    console.log("hello");
+  }
+
+}
--- src/app/helper-files/content-list.ts
+++ src/app/helper-files/content-list.ts
@@ -24,4 +24,32 @@ export class ContentList {
         return this._items.length;
     }
 
+    getContectCard(index: number) : string {
+
+        if (this.lengthContent() <= index){
+            return "<p> error: index is out of range </p>";
+
+        }
+
+        let item = this._items[index];
+
+        return `
+        <div class="card">
+        <h4>${item.title}</h4>
+        <img src="${item.imgURL}" alt="cake" style="width:200px ; heigth: 150px">
+        <p>${item.description}</p>
+        <h5>${item.creator}</h5>
+        <p>${item.type}</p>
+        <hr>
+        </div>
+
+
+
+        `;
+
+
+
+    }
+
+
 }
